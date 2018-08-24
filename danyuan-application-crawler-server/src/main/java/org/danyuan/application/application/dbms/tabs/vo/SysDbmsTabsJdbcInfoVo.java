92c724ca0d6f3ec575e0087c6b8d62e0252c74a5
514840279 <514840279@qq.com>
2018-08-24 17:53:23 +0800

    修改

--- danyuan-application-crawler-server/src/main/java/org/danyuan/application/application/dbms/tabs/vo/SysDbmsTabsJdbcInfoVo.java
+++
@@ -1,8 +0,0 @@
-package org.danyuan.application.application.dbms.tabs.vo;
-
-import org.danyuan.application.application.common.base.Pagination;
-import org.danyuan.application.application.dbms.tabs.po.SysDbmsTabsJdbcInfo;
-
-public class SysDbmsTabsJdbcInfoVo extends Pagination<SysDbmsTabsJdbcInfo> {
-	
-}
--- danyuan-application-crawler-server/src/main/java/org/danyuan/application/application/dbms/tabs/vo/SysDbmsTabsTypeInfoVo.java
+++
@@ -1,8 +0,0 @@
-package org.danyuan.application.application.dbms.tabs.vo;
-
-import org.danyuan.application.application.common.base.Pagination;
-import org.danyuan.application.application.dbms.tabs.po.SysDbmsTabsTypeInfo;
-
-public class SysDbmsTabsTypeInfoVo extends Pagination<SysDbmsTabsTypeInfo> {
-	
-}
